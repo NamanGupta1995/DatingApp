633e728f54b07cdc0dca29627c5e641d4b19e444
Naman Gupta <namangupta1995@gmail.com>
2020-04-29 21:59:08 +0530

    Initial Commit

---
+++ DatingApp.API/Controllers/ValueController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using DatingApp.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace DatingApp.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValueController : ControllerBase
+    {
+        private readonly DataContext _dbContext;
+        public ValueController(DataContext dbContext)
+        {
+            this._dbContext = dbContext;
+
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetValue()
+        {
+            var value = await (from val in _dbContext.Values
+                               select val).ToListAsync();
+
+            return Ok(value);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            var value = await (from val in _dbContext.Values
+                         where val.id == id
+                        select val).FirstOrDefaultAsync();
+
+            return Ok(value);
+        }
+    }
+}
---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,26 @@
+﻿using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        
+        public virtual DbSet<Value> Values { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Value>(entity =>
+            {
+                entity.HasKey(e => e.id);
+                entity.ToTable("Values", "dbo");
+            });
+        }
+
+        }
+}
